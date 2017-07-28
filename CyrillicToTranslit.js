3cecdebffcf124a553fed79420e5cd2cbc9d6cc4
Aleksandr Filatov <v-alfila@microsoft.com>
2017-07-28 15:25:17 -0700

    avoided var

--- CyrillicToTranslit.js
+++ CyrillicToTranslit.js
@@ -1,71 +1,70 @@
 ﻿'use strict';
 
 module.exports = function cyrillicToTranslit() {
-    
-	var _associations = {
-		"а": "a",
-		"б": "b",
-		"в": "v",
-		"ґ": "g",
-		"г": "g",
-		"д": "d",
-		"е": "e",
-		"ё": "e",
-		"є": "ye",
-		"ж": "zh",
-		"з": "z",
-		"и": "i",
-		"і": "i",
-		"ї": "yi",
-		"й": "i",
-		"к": "k",
-		"л": "l",
-		"м": "m",
-		"н": "n",
-		"о": "o",
-		"п": "p",
-		"р": "r",
-		"с": "s",
-		"т": "t",
-		"у": "u",
-		"ф": "f",
-		"x": "h",
-		"ц": "c",
-		"ч": "ch",
-		"ш": "sh",
-		"щ": "sh'",
-		"ъ": "",
-		"ы": "i",
-		"ь": "",
-		"э": "e",
-		"ю": "yu",
-		"я": "ya" 
-	};
-    
-    return {
-        transform: transform
-    }
-    
-    function transform(str, spaceReplacement) {
-        if (!str) {
-            return "";
-        }
+  const _associations = {
+    "а": "a",
+    "б": "b",
+    "в": "v",
+    "ґ": "g",
+    "г": "g",
+    "д": "d",
+    "е": "e",
+    "ё": "e",
+    "є": "ye",
+    "ж": "zh",
+    "з": "z",
+    "и": "i",
+    "і": "i",
+    "ї": "yi",
+    "й": "i",
+    "к": "k",
+    "л": "l",
+    "м": "m",
+    "н": "n",
+    "о": "o",
+    "п": "p",
+    "р": "r",
+    "с": "s",
+    "т": "t",
+    "у": "u",
+    "ф": "f",
+    "x": "h",
+    "ц": "c",
+    "ч": "ch",
+    "ш": "sh",
+    "щ": "sh'",
+    "ъ": "",
+    "ы": "i",
+    "ь": "",
+    "э": "e",
+    "ю": "yu",
+    "я": "ya"
+  };
 
-        var new_str = "";
-        for (var i = 0; i < str.length; i++) {
-            var strLowerCase = str[i].toLowerCase();
-            if (strLowerCase === " " && spaceReplacement) {
-                new_str += spaceReplacement;
-                continue;
-            }
-            var new_letter = _associations[strLowerCase]
-            if ("undefined" === typeof new_letter) {
-                new_str += strLowerCase;
-            }
-            else {
-                new_str += new_letter;
-            }
-        }
-        return new_str;
+  return {
+    transform: transform
+  }
+
+  function transform(str, spaceReplacement) {
+    if (!str) {
+      return "";
     }
+
+    let new_str = "";
+    for (let i = 0; i < str.length; i++) {
+      let strLowerCase = str[i].toLowerCase();
+      if (strLowerCase === " " && spaceReplacement) {
+        new_str += spaceReplacement;
+        continue;
+      }
+      let new_letter = _associations[strLowerCase]
+      if ("undefined" === typeof new_letter) {
+        new_str += strLowerCase;
+      }
+      else {
+        new_str += new_letter;
+      }
+    }
+    return new_str;
+  }
 };
